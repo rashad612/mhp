0ca0740d08d867cdceb8fd47a1dcad1df3c54570
Rashad Majali <rmajali@uba.com.jo>
2016-03-24 16:52:34 +0200

    Init. + red tests.

---
+++ test.js
@@ -0,0 +1,14 @@
+const test = require('tape'),
+      mhp = require('./');
+
+const config = {};
+
+test('custom config', t => {
+  t.ok(false, 'config');
+  t.end();
+}); 
+
+test('server run', t => {
+  t.ok(false, 'server run');
+  t.end();
+});
